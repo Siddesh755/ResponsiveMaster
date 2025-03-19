de56d8a23d044ee65fd24df8c6fb409eb23ecd7c
Siddesh Navthale <Siddesh755@users.noreply.github.com>
2025-03-19 22:29:01 +0530

    Refactor App component layout and add contact information

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ucjpg from "./assets/uc.jpg";
 function App() {
   return (
-    <div className='flex justify-center -mt-4'>
-      <img src={ucjpg} alt=" uc" class="w-96 h-96" />
+    <div className='flex flex-col justify-center item items-center'>
+      <img src={ucjpg} alt=" uc"  className='flex justify-center w-5/12 h-5/12 -mt-8' />
+      <div className='flex  flex-col items-center '>
+        <h1>for more details contact </h1>
+        <a href="https://www.linkedin.com/in/siddesh-navthale-38b555230/" class=" text-gray-700 font-bold text-4xl"> Siddesh Navthale</a>
       </div>
+      </div>
+      
   )
 }
 
